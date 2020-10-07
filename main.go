3010c69e0bc267a9059f59d9937eb361af7c26ed
jamie <jamie@163.com>
2020-10-07 13:06:13 +0800

    feature: 添加用户查询逻辑

--- main.go
+++ main.go
@@ -8,6 +8,11 @@ import (
 
 // 主方法，用于启动go程序
 func main() {
+	// 静态资源处理
+	// http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(assets.AssetFS())))
+	http.Handle("/static/",
+		http.StripPrefix("/static/", http.FileServer(http.Dir("./static"))))
+
 	// 转到上传文件的页面
 	http.HandleFunc("/file/upload", handler.UploadHandler)
 	// 上传文件成功后返回的参数
@@ -20,6 +25,12 @@ func main() {
 	http.HandleFunc("/file/update", handler.FileMetaUpdateHandler)
 	// 删除上传的文件
 	http.HandleFunc("/file/delete", handler.FileDeleteHandler)
+	//http.HandleFunc("/file/query  ", handler.FileQueryHandler)
+
+	// 用户注册
+	http.HandleFunc("/user/signup", handler.SignupHandler)
+	http.HandleFunc("/user/signin", handler.SignInHandler)
+	http.HandleFunc("/user/info", handler.UserInfoHandler)
 	// 监听端口
 	err := http.ListenAndServe(":8080", nil)
 	if err != nil {
